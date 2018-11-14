4134eebe854c23e9a08b059d345ebc1d8937cd66
Shane Moodie <shanepmoodie96@gmail.com>
2018-11-14 11:32:33 -0500

    New Vendor Page Done

---
+++ CuttingEdgeLighting/Controllers/NewVendorsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CuttingEdgeLighting.Models;
+
+namespace CuttingEdgeLighting.Controllers
+{
+    public class NewVendorsController : Controller
+    {
+        private NewVendorContext db = new NewVendorContext();
+
+        // GET: NewVendors
+        public ActionResult Questionnaire()
+        {
+            return View(db.NewVendors.ToList());
+        }
+
+      
+    }
+}
---
+++ CuttingEdgeLighting/Models/NewVendor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CuttingEdgeLighting.Models
+{
+    public class NewVendor
+    {
+        public int ID { get; set; }
+    }
+}
