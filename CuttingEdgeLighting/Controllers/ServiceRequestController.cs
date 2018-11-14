391f4ae7fc62cdefc66135806e2606df92b46ce3
Shane Moodie <shanepmoodie96@gmail.com>
2018-11-14 16:20:18 -0500

    ServiceRequest Page halfway

---
+++ CuttingEdgeLighting/Controllers/ServiceRequestController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CuttingEdgeLighting.Controllers
+{
+    public class ServiceRequestController : Controller
+    {
+        // GET: ServiceRequest
+        public ActionResult ServiceRequest()
+        {
+            return View();
+        }
+    }
+}
