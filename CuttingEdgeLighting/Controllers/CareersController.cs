89a929c2eee0db00fcee398b5808fae569738a6d
Shane Moodie <shanepmoodie96@gmail.com>
2018-11-15 16:07:18 -0500

    Started Career Page

---
+++ CuttingEdgeLighting/Controllers/CareersController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CuttingEdgeLighting.Controllers
+{
+    public class CareersController : Controller
+    {
+        // GET: Careers
+        public ActionResult Careers()
+        {
+            return View();
+        }
+    }
+}
