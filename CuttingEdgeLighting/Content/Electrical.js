028efc5688740d9ff3f21b7f260f9e1bb1dae4ca
Shane Moodie <shanepmoodie96@gmail.com>
2018-11-29 13:38:40 -0500

    Finished changes, started National Accounts

--- CuttingEdgeLighting/Content/Electrical.js
+++ CuttingEdgeLighting/Content/Electrical.js
@@ -6,7 +6,6 @@ function changeTextOverviewElec() {
     jsCovArea.style.display = 'none';
     jsQuote.style.display = 'none';
     jsOverview.style.display = 'block';
-    jsOverview.innerHTML = '<h1>Electrical Services:</h1><br /><p>Cutting-Edge is a full-service electrical contractor that has been providing a full range of commercial and industrial services. We are a dedicated company offering national reach with licensed electrical contractors in the US and national buying power with the personalized service of a local contractor.<br /><br />Proven performance and strong relationships mean we have the experience and qualified manpower to tackle any size project. From electrical service and maintenance on a local or nationwide scale, to new construction. <br /><br /><b>Cutting Edge Lighting is here for our customers 24 hours a day for all your electrical needs.</b><br /><br /></p>';
 }
 
 function changeTextCoverage() {
--- CuttingEdgeLighting/Content/EnergyPage.js
+++ CuttingEdgeLighting/Content/EnergyPage.js
@@ -1,4 +1,6 @@
 ﻿var jsOverview = document.getElementById('overview');
+var jsEms = document.getElementById('ems?');
+var jsCount = document.getElementById('count');
 var jsCovArea = document.getElementById('coverageArea');
 var jsQuote = document.getElementById('quote');
 
@@ -6,30 +8,37 @@ function changeTextOverviewEnergy() {
     jsCovArea.style.display = 'none';
     jsQuote.style.display = 'none';
     jsOverview.style.display = 'block';
-    jsOverview.innerHTML = '<h1>Energy Services:</h1><br /><p>Cutting Edges Energy Management Systems will keep your rooms warm, your freezers cold, and your lights bright.Not sure what an E.M.S is or what system would be right for you and your company ? Continue reading for more information, and feel free to fill out a quote form to beign your journey with Cutting Edge Lighting Solutions!</p>';
+    jsEms.style.display = 'none';
+    jsCount.style.display = 'none';
 }
 function changeTextEMS() {
     jsCovArea.style.display = 'none';
     jsQuote.style.display = 'none';
-    jsOverview.style.display = 'block';
-    jsOverview.innerHTML = '<h1>What is E.M.S?</h1><br /><p>An EMS or Building Management System (BMS) as it is also called, is a computer-based control system installed in buildings that controls and monitors its mechanical and electrical equipment, such as ventilation, lighting, power systems, fire systems, and security systems. One of the primary objectives of a BMS is to reduce costs through improved operational and energy efficiency, among other uses, including:<br /><br />• Control of internal comfort conditions<br />• Individual room or office control<br />• Effective proactive monitoring and targeting of energy consumption<br />• Improved reliability and life of mechanical systems<br />• Effective response to HVAC-related complaints<br /><br />A traditional BMS consists of software and hardware; the software program is usually configured in a hierarchical manner with multiple levels of networks and is typically proprietary. All BMS systems integrate Internet protocols and open standards such as BACnet (Building Automation Control network), LON (local operating networks), and Modbus, which allow standardized communications between building automation devices from different hardware manufacturers. These systems are mainly custom and are installed in a office buildings, campus type universities, etc.<br /><br />One of the largest benefits that Cutting Edge is known for is selecting the right system for the customer. Many companies purchase an EMS/BMS system that is overkill for their facility and usually is disconnected or override within several years and turns out to be a waste of money.<br /><br />Cutting Edge’s Energy Management and Building Controls Systems & Services Solution assists companies select the right system for their operation. From a single location to multi-site, multi-state facilities―with designing and implementing energy management and building controls systems, including engineering, installation, commissioning and system maintenance, as well as overall project management.<br /><br />We enjoy the challenge of working with clients to make sure that they are installing the right system for their needs and more importanly use the data to make informed decisions. It’s a great reward to see the benefit and value the client receives.<br /><br />Please contact us with any questions or for assistance in creating a EMS / BMS that’s right for you.</p>';
+    jsOverview.style.display = 'none';
+    jsEms.style.display = 'block';
+    jsCount.style.display = 'none';
 }
 
 function changeTextCount() {
     jsCovArea.style.display = 'none';
     jsQuote.style.display = 'none';
-    jsOverview.style.display = 'block';
-    jsOverview.innerHTML = '<h1>You Can Count on Cutting Edge Lighting Solutions!</h1><br /><p>You can count on Cutting Edge Lighting Co. to help you create an environment that’s more comfortable and pleasing to customers, more productive for employees, and better for your community. The Cutting-Edge Solution has helped customers throughout the country improve the efficiency of their buildings and operations every day.<br /><br />Continually rising energy prices puts real pressure on a multi-site business. An energy management system or a lighting retrofit helps curtail energy usage, lower energy bills, extend HVAC equipment life and optimize customer and employee comfort through advanced scheduling, controlling and monitoring of HVAC and lighting consumption.<br /><br />After conducting extensive testing in various environments across the country, The Cutting Edge Solution has been proven to increase operating efficiency and lower energy consumption and costs through a sustainable, end-to-end energy management solution that offers visibility and control from one central point.<br /><br /></p><h4>Anywhere Accessibility:<br /></h4><p>Schedule, control and monitor your HVAC, lighting and other energy loads either on-site or through the internet or from your Smartphone .<br /></p><h4>Rapid Return on Investment:<br /></h4><p>Confirmed data has demonstrated that Siemens will reduce HVAC and lighting consumption by and guarantee it!<br /></p><h4>Fast & Easy Installation:<br /></h4><p>Over 10,000 installations and we have never lost a customer! We ensure no downtime or disruptions at your site. This means you start saving immediately and decrease your operating costs.<br /></p><h4>Scalable to Your Needs:<br /></h4><p>Centralized visibility monitors multiple locations, today or when expanding in the future.<br /><br />A Proven system that has been rigorously tested in locations across the United States, Canada and South America with proven results time after time.</p><br /><b>Contact us today to see why thousands of retailers have made the switch to Cutting Edge Lighting Solutions!</b><br />';
+    jsOverview.style.display = 'none';
+    jsEms.style.display = 'none';
+    jsCount.style.display = 'block';
 } 
 
 function changeTextCoverage() {
     jsQuote.style.display = 'none';
     jsOverview.style.display = 'none';
     jsCovArea.style.display = 'block';
+    jsEms.style.display = 'none';
+    jsCount.style.display = 'none';
 }
 
 function changeTextQuote() {
     jsCovArea.style.display = 'none';
     jsOverview.style.display = 'none';
     jsQuote.style.display = 'block';
+    jsEms.style.display = 'none';
+    jsCount.style.display = 'none';
 }
